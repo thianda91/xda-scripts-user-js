3563495561db556abd53eff47a6c8893c32aa5da
yuxianda <yxd9721@qq.com>
2018-06-22 18:10:27 +0800

    v0.3.3

--- beian-chinamobile.user.js
+++ beian-chinamobile.user.js
@@ -3,14 +3,14 @@
 // @description		本脚本用于beian.chinamobile.com界面优化。1.美化UI，响应式布局，消灭滚动条。2.整合为单页应用，无刷新查询，无跳转实现批量上传（左下角）。3.后续会增加自动化功能，新增或修改后自动上报。
 // @author			Xianda
 // @create			2018-06-10
-// @version			0.3.2
+// @version			0.3.3
 // @match			*://beian.chinamobile.com/*
 // @namespace		beian-chinamobile
 // @license			MIT
 // @copyright		2018, Xianda
-// @lastmodified	2018-06-19
+// @lastmodified	2018-06-22
 // @feedback-url	https://greasyfork.org/zh-CN/scripts/369426
-// @note			2018-06-19-V0.2.0	修复bug
+// @note			2018-06-21-V0.3.2	修复bug
 // @note			2018-06-19-V0.2.0	测试脚本无法自动更新
 // @note			2018-06-13-V0.1.7	修复bug
 // @note			2018-06-13-V0.1.5	调整布局，查询错误时支持显示错误提示
@@ -29,7 +29,7 @@
 (function() {
 	'use strict';
 
-	var devVersion = "0.3.2";
+	var devVersion = "0.3.3";
 
 	// Ajax 特效
 	$("body").append('<style>.head{background:#94aedb}#load{position:absolute;top:0;bottom:0;left:0;right:0;z-index:200;}#load ._close{position:absolute;bottom:20px;left:0;height:50px;width:50px;font-size:100px;color:#000;cursor:pointer;line-height:50px;opacity:.2}.spinner{position:absolute;top:50%;left:50%;margin-top:-100px;margin-left:-300px;text-align:center}.spinner>div{width:200px;height:200px;background-color:#67CF22;border-radius:100%;display:inline-block;-webkit-animation:bouncedelay 1.4s infinite ease-in-out;animation:bouncedelay 1.4s infinite ease-in-out;-webkit-animation-fill-mode:both;animation-fill-mode:both}.spinner .bounce1{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce2{-webkit-animation-delay:-.16s;animation-delay:-.16s}@-webkit-keyframes bouncedelay{0%,80%,100%{-webkit-transform:scale(0)}40%{-webkit-transform:scale(1)}}@keyframes bouncedelay{0%,80%,100%{transform:scale(0);-webkit-transform:scale(0)}40%{transform:scale(1);-webkit-transform:scale(1)}}</style><div id="load"><div class="_close" onclick="document.getElementById(&quot;load&quot;).style.display=&quot;none&quot;">×</div><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
@@ -113,7 +113,7 @@
 		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").html($(".header .user").html());
 		//$("#major-content table.t-detail tr:eq(1) td:eq(3) .exit").css({"background":"url(../../images/v3/exit.gif) no-repeat","padding-left":"20px","color":"#102c93","margin":"0 20px"});
 		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").css("text-align", "right");
-		$("#major-content table.t-detail tr:eq(4) td:eq(3)").html('Version: ' + devVersion).css("clor","#");
+		$("#major-content table.t-detail tr:eq(4) td:eq(3)").html('Version: ' + devVersion);
 
 		// 添加提示
 		$("#major-content table.t-detail tr:eq(4) td:eq(1)").html($("#major-content table.t-detail tr:eq(4) td:eq(1)").html().replace('格式：192.168.1.1', '<span style="color:red">结束ip留空，查询时会自动和起始ip相同</span>'));
@@ -132,9 +132,7 @@
 			window.mysumitAdd = function(){
 				var file= document.fp_xx_new_fm.file2.value;
 				if (file!=""&&file!=null){
-					document.getElementById("piliang").disabled = true;
 					document.fp_xx_new_fm.submit();
-					document.getElementById('piliang').value = '正在导入中...';
 				}
 				else{
 					alert("未选导入文件");
