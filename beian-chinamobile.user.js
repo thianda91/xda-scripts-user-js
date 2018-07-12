5ff1d072d498707c83265ffd03ef7ad41b05a827
thianda <yxd9721@qq.com>
2018-07-12 21:50:51 +0800

    v0.6.2 remove require jquery1.7.2

--- beian-chinamobile.user.js
+++ beian-chinamobile.user.js
@@ -7,16 +7,16 @@
 // @description:zh-CN	本脚本用于beian.chinamobile.com界面优化。1.美化UI，响应式布局，消灭滚动条。2.整合为单页应用，无刷新查询，无跳转实现批量上传（左下角）。3.后续会增加自动化功能，新增或修改后自动上报。
 // @author				X.Da
 // @create				2018-06-10
-// @version				0.6.1
+// @version				0.6.2
 // @match				*://beian.chinamobile.com/*
 // @match				*://10.1.68.22/*
 // @match				*://10.1.68.37/*
 // @namespace			beian-chinamobile
 // @license				MIT
 // @copyright			2018, X.Da
-// @lastmodified		2018-07-10
+// @lastmodified		2018-07-12
 // @feedback-url		https://greasyfork.org/scripts/369426
-// @note				2018-07-10-V0.6.1	修复bug
+// @note				2018-07-12-V0.6.2	修复bug
 // @note				2018-07-10-V0.6.0	新增对集团安全管控平台（集团IP备案）的优化
 // @note				2018-07-09-V0.5.0	修复批量修改按钮功能无效bug
 // @note				2018-06-25-V0.4.1	查询页面的input添加autocomplete="off"，去除自动完成，非查询页面提示脚本未生效。
@@ -31,17 +31,16 @@
 // @note				2018-06-11-V0.1.1	支持记录本地登陆信息
 // @note				2018-06-10-V0.1.0	支持查询
 // @homepage			https://github.com/thianda/beian-chinamobile
-// @require				https://cdn.bootcss.com/jquery/1.7.2/jquery.min.js
 // @icon				http://wx2.sinaimg.cn/large/679a709ely1frs5u2z5ibj205c05c3yd.jpg
 // @downloadURL			https://greasyfork.org/scripts/369426-beian-chinamobile-beautifier/code/beian-chinamobile-beautifier.user.js
-// @run-at				document-body
+// @run-at				document-end
 // @grant				none
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	var devVersion = "0.6.1";
+	var devVersion = "0.6.2";
 
 	// Ajax 特效
 	$("body").append('<style>.head{background:#94aedb}#load{position:absolute;top:0;bottom:0;left:0;right:0;z-index:200;}#load ._close{position:absolute;bottom:20px;left:0;height:50px;width:50px;font-size:100px;color:#000;cursor:pointer;line-height:50px;opacity:.2}.spinner{position:absolute;top:50%;left:50%;margin-top:-100px;margin-left:-300px;text-align:center}.spinner>div{width:200px;height:200px;background-color:#67CF22;border-radius:100%;display:inline-block;-webkit-animation:bouncedelay 1.4s infinite ease-in-out;animation:bouncedelay 1.4s infinite ease-in-out;-webkit-animation-fill-mode:both;animation-fill-mode:both}.spinner .bounce1{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce2{-webkit-animation-delay:-.16s;animation-delay:-.16s}@-webkit-keyframes bouncedelay{0%,80%,100%{-webkit-transform:scale(0)}40%{-webkit-transform:scale(1)}}@keyframes bouncedelay{0%,80%,100%{transform:scale(0);-webkit-transform:scale(0)}40%{transform:scale(1);-webkit-transform:scale(1)}}</style><div id="load"><div class="_close" onclick="document.getElementById(&quot;load&quot;).style.display=&quot;none&quot;">×</div><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
@@ -54,8 +53,8 @@
 	$(function() {
 		$("#load").hide();
 
-		// 集团安全管控平台(集团IP备案)相关优化
-		// 登录页
+		/* 集团安全管控平台(集团IP备案)相关优化 */
+		// 集团安全管控登录页
 		if(document.location.host == "10.1.68.37:8080"){
 			if(document.location.href.match("indexForJT.jsp")){
 				document.forms[0].smsName.value = localStorage.getItem("xda_username");
@@ -66,7 +65,7 @@
 			return false;
 		}
 
-		// 登陆后首页
+		// 集团安全管控登陆后首页
 		if(document.location.host == "10.1.68.22"){
 			// 自动打开ctrix
 			window.autoOpenCtrix = function (){
@@ -96,7 +95,7 @@
 			return false;
 		}
 
-		// 首页
+		// 工信部备案首页
 		if (document.myform && myform.action.match("/user/login.jhtml")) {
 			//$("td[colspan=3]:first").removeAttr("colspan").after("<td colspan=2><b style='color:green'>已启用优化，登录后享受！</b></td>");
 			//$("table table table tr:eq(1)").append("<td>V"+devVersion+"</td>");
