3ac0bb462c2b9ae2580635f182afafcd8468a7ff
thianda <yxd9721@qq.com>
2018-07-12 22:27:01 +0800

    v0.6.3 auto prevent going offline

--- beian-chinamobile.user.js
+++ beian-chinamobile.user.js
@@ -7,7 +7,7 @@
 // @description:zh-CN	本脚本用于beian.chinamobile.com界面优化。1.美化UI，响应式布局，消灭滚动条。2.整合为单页应用，无刷新查询，无跳转实现批量上传（左下角）。3.后续会增加自动化功能，新增或修改后自动上报。
 // @author				X.Da
 // @create				2018-06-10
-// @version				0.6.2
+// @version				0.6.3
 // @match				*://beian.chinamobile.com/*
 // @match				*://10.1.68.22/*
 // @match				*://10.1.68.37/*
@@ -16,7 +16,7 @@
 // @copyright			2018, X.Da
 // @lastmodified		2018-07-12
 // @feedback-url		https://greasyfork.org/scripts/369426
-// @note				2018-07-12-V0.6.2	修复bug
+// @note				2018-07-12-V0.6.3	修复bug
 // @note				2018-07-10-V0.6.0	新增对集团安全管控平台（集团IP备案）的优化
 // @note				2018-07-09-V0.5.0	修复批量修改按钮功能无效bug
 // @note				2018-06-25-V0.4.1	查询页面的input添加autocomplete="off"，去除自动完成，非查询页面提示脚本未生效。
@@ -40,7 +40,7 @@
 (function() {
 	'use strict';
 
-	var devVersion = "0.6.2";
+	var devVersion = "0.6.3";
 
 	// Ajax 特效
 	$("body").append('<style>.head{background:#94aedb}#load{position:absolute;top:0;bottom:0;left:0;right:0;z-index:200;}#load ._close{position:absolute;bottom:20px;left:0;height:50px;width:50px;font-size:100px;color:#000;cursor:pointer;line-height:50px;opacity:.2}.spinner{position:absolute;top:50%;left:50%;margin-top:-100px;margin-left:-300px;text-align:center}.spinner>div{width:200px;height:200px;background-color:#67CF22;border-radius:100%;display:inline-block;-webkit-animation:bouncedelay 1.4s infinite ease-in-out;animation:bouncedelay 1.4s infinite ease-in-out;-webkit-animation-fill-mode:both;animation-fill-mode:both}.spinner .bounce1{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce2{-webkit-animation-delay:-.16s;animation-delay:-.16s}@-webkit-keyframes bouncedelay{0%,80%,100%{-webkit-transform:scale(0)}40%{-webkit-transform:scale(1)}}@keyframes bouncedelay{0%,80%,100%{transform:scale(0);-webkit-transform:scale(0)}40%{transform:scale(1);-webkit-transform:scale(1)}}</style><div id="load"><div class="_close" onclick="document.getElementById(&quot;load&quot;).style.display=&quot;none&quot;">×</div><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
@@ -48,162 +48,6 @@
 		$("#load").show();
 	}).ajaxStop(function() {
 		$("#load").hide();
-	});
-
-	$(function() {
-		$("#load").hide();
-
-		/* 集团安全管控平台(集团IP备案)相关优化 */
-		// 集团安全管控登录页
-		if(document.location.host == "10.1.68.37:8080"){
-			if(document.location.href.match("indexForJT.jsp")){
-				document.forms[0].smsName.value = localStorage.getItem("xda_username");
-				$("#getPasswd").bind("click",function(){
-					localStorage.setItem("xda_username", document.forms[0].smsName.value);
-				});
-			}
-			return false;
-		}
-
-		// 集团安全管控登陆后首页
-		if(document.location.host == "10.1.68.22"){
-			// 自动打开ctrix
-			window.autoOpenCtrix = function (){
-				if(window.frames["content"].document.getElementsByClassName("optionbtn-left").length > 0){
-					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderWidth = "1px";
-					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderColor = "red";
-					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderStyle = "solid";
-					(function bbb(){
-						var cc = sessionStorage.getItem("xda_cc") | 0;
-						var bgcolor = ["#fb002d","#1b87b8","#000","#f83cd4"];
-						$(window.frames["content"].document.getElementsByClassName("optionbtn-left")[0]).css("background", bgcolor[cc%4]);
-						sessionStorage.setItem("xda_cc", ++cc);
-						//console.log("cc:\t",cc,bgcolor,Date.now());
-						if(cc < 64){
-							setTimeout(function(){bbb()}, 10);
-						}else{
-							$(window.frames["content"].document.getElementsByClassName("optionbtn-left")[0]).css("background", "#8fc31f");
-							window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].click();
-							sessionStorage.setItem("xda_cc",null);
-						}
-					})();
-				}else{
-					setTimeout(autoOpenCtrix,1000);
-				}
-			}
-			autoOpenCtrix();
-			return false;
-		}
-
-		// 工信部备案首页
-		if (document.myform && myform.action.match("/user/login.jhtml")) {
-			//$("td[colspan=3]:first").removeAttr("colspan").after("<td colspan=2><b style='color:green'>已启用优化，登录后享受！</b></td>");
-			//$("table table table tr:eq(1)").append("<td>V"+devVersion+"</td>");
-			$("a.more").after('<div style="float:right;color:#fff;background-color:#000;margin-right:160px;padding:0 10px;">已启动优化,登录后查看！(V'+devVersion+')</div>');
-			myform.userType.value = 2;
-			window.myclick = function() {
-				localStorage.setItem("xda_username", myform.userName.value);
-				localStorage.setItem("xda_password", myform.password.value);
-				var password = $("#password");
-				var count = $("#count");
-				if (count.val() == "") {
-					password.val($.md5(password.val()));
-				}
-				return;
-			}
-			var _username = localStorage.getItem("xda_username");
-			var _password = localStorage.getItem("xda_password");
-			if (_username && _password) {
-				login(_username, _password);
-			}
-			myform.authCode.autocomplete="off";
-			myform.authCode.value="";
-			myform.authCode.focus();
-			return;
-		}
-
-		// 登录函数，本地自动登录
-		function login(_username, _password) {
-			myform.userName.value = _username;
-			myform.password.value = _password;
-		}
-
-		// 自动跳转到查询页
-		if (location.href.match("//beian.chinamobile.com/ismmobile/index/index.jhtml")) {
-			location.href = "/ismmobile/ipbak/fp_xx_list.jhtml";
-			return;
-		}
-
-		// 若不是查询页则结束执行
-		if(!location.href.match("ipbak/fp_xx_list.jhtml")){
-			$("#NavBar").append('<p style="color:#fff;background-color:#000;margin-top:50px;padding:0 10px;">您已离开查询页面，优化脚本暂不生效！(V'+devVersion+')</p><a class="button" href="fp_xx_list.jhtml" style="display:inline-block;">返回查询页面</a>');
-			return;
-		}
-
-		// 调整header
-		//document.getElementById("Container").style.marginTop = 0;
-		//document.getElementsByClassName("header")[0].style.display = "none";
-		document.getElementById("Container").style.marginTop = "70px";
-		document.getElementsByClassName("header")[0].style.zIndex = "-1";
-
-		// 去除子标题
-		//document.getElementsByClassName("subtitle")[0].style.display = "none";
-		$(".subtitle").remove();
-
-		// 隐藏左侧菜单
-		//document.getElementById("NavBar").style.display = "none";
-		$("#NavBar").remove();
-
-		// 扩展显示主div到左侧边缘
-		document.getElementById("MainBody").style.marginLeft = 0;
-
-		// ip查询页(fp_xx_list.jhtml) 隐藏不常用的条件输入框
-		//if(location.pathname.indexOf("ismmobile/ipbak/fp_xx_list.jhtml")>0){
-		var __uselessLine = [0, 2, 5, 6, 7];
-		for (var i in __uselessLine) {
-			$("#major-content table.t-detail tr:eq(" + __uselessLine[i] + ")").hide();
-		}
-
-		// 调整 查询表单 的位置
-		$("#major-content table.t-detail tr:eq(1) td:eq(1)").append($("#major-content table.t-detail tr:eq(1) td:eq(2)").text()).append($("#major-content table.t-detail tr:eq(1) td:eq(3) select"));
-		$("#major-content table.t-detail tr:eq(1) td:eq(2)").html('<span style="color: #088bff;font-size: 14px;">UI优化：<a href="https://github.com/thianda" target="_blank">X.Da</a></span>');
-		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").html($(".header .user").html());
-		//$("#major-content table.t-detail tr:eq(1) td:eq(3) .exit").css({"background":"url(../../images/v3/exit.gif) no-repeat","padding-left":"20px","color":"#102c93","margin":"0 20px"});
-		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").css("text-align", "right");
-		$("#major-content table.t-detail tr:eq(4) td:eq(3)").html('<a href="https://greasyfork.org/zh-CN/scripts/369426-beian-chinamobile-beautifier/versions" target="_blank">Version</a>: ' + devVersion);
-
-		// 添加提示
-		$("#major-content table.t-detail tr:eq(4) td:eq(1)").html($("#major-content table.t-detail tr:eq(4) td:eq(1)").html().replace('格式：192.168.1.1', '<span style="color:red">结束ip留空，查询时会自动和起始ip相同</span>'));
-		$("#major-content table.t-detail tr:eq(4) td:eq(2)").text("");
-		$("#major-content table.t-detail tr:eq(8) td:eq(1) input:eq(1)").remove();
-		$("#major-content table.t-detail tr:eq(8) td:eq(1)").append('<a class="button" href="fp_xx_new.jhtml" target="_blank" style=" display:inline-block;">新增</a>');
-		$("#major-content table.t-detail input").attr("autocomplete","off");
-
-		// 取消搜索结果div的宽度限制
-		adjustDataTable();
-		
-		// 添加批量操作功能到当前页
-		initPiLiang();
-
-		// 隐藏上方的批量操作按钮
-		//$("#major-content table tr:eq(8) td.value input")[2].style.display = "none";
-		$("#major-content table tr:eq(8) td.value input:eq(2)").remove();
-
-		// 防掉线
-		$("#major-content form[name='qvo_fm'] td:last").prepend('<button id="prevDown" class="button" data-txt="no">开启防掉线</button>');
-		$("#prevDown").bind("click",function(){
-			var tpl = {no:['yes','关闭防掉线'],yes:['no','开启防掉线']};
-			var _status = $(this).attr("data-txt");
-			if(_status=='no'){
-				flip(this);
-			}else if(_status=='yes'){
-				clearTimeout(this.flip);
-			}
-			$(this).text(tpl[_status][1]);
-			$(this).attr("data-txt",tpl[_status][0]);
-			return false;
-		});
-
 	});
 
 	// 随机setInterval函数
@@ -417,4 +261,161 @@
 		}
 		return queryComponents.join('&');
 	}
+	
+	$(function() {
+		$("#load").hide();
+
+		/* 集团安全管控平台(集团IP备案)相关优化 */
+		// 集团安全管控登录页
+		if(document.location.host == "10.1.68.37:8080"){
+			if(document.location.href.match("indexForJT.jsp")){
+				document.forms[0].smsName.value = localStorage.getItem("xda_username");
+				$("#getPasswd").bind("click",function(){
+					localStorage.setItem("xda_username", document.forms[0].smsName.value);
+				});
+			}
+			return false;
+		}
+
+		// 集团安全管控登陆后首页
+		if(document.location.host == "10.1.68.22"){
+			// 自动打开ctrix
+			window.autoOpenCtrix = function (){
+				if(window.frames["content"].document.getElementsByClassName("optionbtn-left").length > 0){
+					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderWidth = "1px";
+					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderColor = "red";
+					window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].style.borderStyle = "solid";
+					(function bbb(){
+						var cc = sessionStorage.getItem("xda_cc") | 0;
+						var bgcolor = ["#fb002d","#1b87b8","#000","#f83cd4"];
+						$(window.frames["content"].document.getElementsByClassName("optionbtn-left")[0]).css("background", bgcolor[cc%4]);
+						sessionStorage.setItem("xda_cc", ++cc);
+						//console.log("cc:\t",cc,bgcolor,Date.now());
+						if(cc < 64){
+							setTimeout(function(){bbb()}, 10);
+						}else{
+							$(window.frames["content"].document.getElementsByClassName("optionbtn-left")[0]).css("background", "#8fc31f");
+							window.frames["content"].document.getElementsByClassName("optionbtn-left")[0].click();
+							sessionStorage.setItem("xda_cc",null);
+						}
+					})();
+				}else{
+					setTimeout(autoOpenCtrix,1000);
+				}
+			}
+			autoOpenCtrix();
+			return false;
+		}
+
+		// 工信部备案首页
+		if (document.myform && myform.action.match("/user/login.jhtml")) {
+			//$("td[colspan=3]:first").removeAttr("colspan").after("<td colspan=2><b style='color:green'>已启用优化，登录后享受！</b></td>");
+			//$("table table table tr:eq(1)").append("<td>V"+devVersion+"</td>");
+			$("a.more").after('<div style="float:right;color:#fff;background-color:#000;margin-right:160px;padding:0 10px;">已启动优化,登录后查看！(V'+devVersion+')</div>');
+			myform.userType.value = 2;
+			window.myclick = function() {
+				localStorage.setItem("xda_username", myform.userName.value);
+				localStorage.setItem("xda_password", myform.password.value);
+				var password = $("#password");
+				var count = $("#count");
+				if (count.val() == "") {
+					password.val($.md5(password.val()));
+				}
+				return;
+			}
+			var _username = localStorage.getItem("xda_username");
+			var _password = localStorage.getItem("xda_password");
+			if (_username && _password) {
+				login(_username, _password);
+			}
+			myform.authCode.autocomplete="off";
+			myform.authCode.value="";
+			myform.authCode.focus();
+			return;
+		}
+
+		// 登录函数，本地自动登录
+		function login(_username, _password) {
+			myform.userName.value = _username;
+			myform.password.value = _password;
+		}
+
+		// 自动跳转到查询页
+		if (location.href.match("//beian.chinamobile.com/ismmobile/index/index.jhtml")) {
+			location.href = "/ismmobile/ipbak/fp_xx_list.jhtml";
+			return;
+		}
+
+		// 若不是查询页则结束执行
+		if(!location.href.match("ipbak/fp_xx_list.jhtml")){
+			$("#NavBar").append('<p style="color:#fff;background-color:#000;margin-top:50px;padding:0 10px;">您已离开查询页面，优化脚本暂不生效！(V'+devVersion+')</p><a class="button" href="fp_xx_list.jhtml" style="display:inline-block;">返回查询页面</a>');
+			return;
+		}
+
+		// 调整header
+		//document.getElementById("Container").style.marginTop = 0;
+		//document.getElementsByClassName("header")[0].style.display = "none";
+		document.getElementById("Container").style.marginTop = "70px";
+		document.getElementsByClassName("header")[0].style.zIndex = "-1";
+
+		// 去除子标题
+		//document.getElementsByClassName("subtitle")[0].style.display = "none";
+		$(".subtitle").remove();
+
+		// 隐藏左侧菜单
+		//document.getElementById("NavBar").style.display = "none";
+		$("#NavBar").remove();
+
+		// 扩展显示主div到左侧边缘
+		document.getElementById("MainBody").style.marginLeft = 0;
+
+		// ip查询页(fp_xx_list.jhtml) 隐藏不常用的条件输入框
+		//if(location.pathname.indexOf("ismmobile/ipbak/fp_xx_list.jhtml")>0){
+		var __uselessLine = [0, 2, 5, 6, 7];
+		for (var i in __uselessLine) {
+			$("#major-content table.t-detail tr:eq(" + __uselessLine[i] + ")").hide();
+		}
+
+		// 调整 查询表单 的位置
+		$("#major-content table.t-detail tr:eq(1) td:eq(1)").append($("#major-content table.t-detail tr:eq(1) td:eq(2)").text()).append($("#major-content table.t-detail tr:eq(1) td:eq(3) select"));
+		$("#major-content table.t-detail tr:eq(1) td:eq(2)").html('<span style="color: #088bff;font-size: 14px;">UI优化：<a href="https://github.com/thianda" target="_blank">X.Da</a></span>');
+		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").html($(".header .user").html());
+		//$("#major-content table.t-detail tr:eq(1) td:eq(3) .exit").css({"background":"url(../../images/v3/exit.gif) no-repeat","padding-left":"20px","color":"#102c93","margin":"0 20px"});
+		//$("#major-content table.t-detail tr:eq(1) td:eq(3)").css("text-align", "right");
+		$("#major-content table.t-detail tr:eq(4) td:eq(3)").html('<a href="https://greasyfork.org/zh-CN/scripts/369426-beian-chinamobile-beautifier/versions" target="_blank">Version</a>: ' + devVersion);
+
+		// 添加提示
+		$("#major-content table.t-detail tr:eq(4) td:eq(1)").html($("#major-content table.t-detail tr:eq(4) td:eq(1)").html().replace('格式：192.168.1.1', '<span style="color:red">结束ip留空，查询时会自动和起始ip相同</span>'));
+		$("#major-content table.t-detail tr:eq(4) td:eq(2)").text("");
+		$("#major-content table.t-detail tr:eq(8) td:eq(1) input:eq(1)").remove();
+		$("#major-content table.t-detail tr:eq(8) td:eq(1)").append('<a class="button" href="fp_xx_new.jhtml" target="_blank" style=" display:inline-block;">新增</a>');
+		$("#major-content table.t-detail input").attr("autocomplete","off");
+
+		// 取消搜索结果div的宽度限制
+		adjustDataTable();
+		
+		// 添加批量操作功能到当前页
+		initPiLiang();
+
+		// 隐藏上方的批量操作按钮
+		//$("#major-content table tr:eq(8) td.value input")[2].style.display = "none";
+		$("#major-content table tr:eq(8) td.value input:eq(2)").remove();
+
+		// 防掉线
+		$("#major-content form[name='qvo_fm'] td:last").prepend('<button id="prevDown" class="button" data-txt="no">开防掉线</button>');
+		$("#prevDown").bind("click",function(){
+			var tpl = {no:['yes','关闭防掉线'],yes:['no','开启防掉线']};
+			var _status = $(this).attr("data-txt");
+			if(_status=='no'){
+				flip(this);
+			}else if(_status=='yes'){
+				clearTimeout(this.flip);
+			}
+			$(this).text(tpl[_status][1]);
+			$(this).attr("data-txt",tpl[_status][0]);
+			return false;
+		});
+		$("#prevDown").click();
+
+	});
 })();
