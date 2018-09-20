e590af4a4ebf38808720221281378a7a2da5413d
thianda <thianda91@outlook.com>
2018-09-20 09:46:09 +0800

    v0.7.6 fixed autoDelAndPost logic

--- beian-chinamobile.user.js
+++ beian-chinamobile.user.js
@@ -7,15 +7,16 @@
 // @description:zh-CN	本脚本用于beian.chinamobile.com界面优化。1.美化UI，响应式布局，消灭滚动条。2.整合为单页应用，无刷新查询，无跳转实现批量上传（左下角）。3.后续会增加自动化功能，新增或修改后自动上报。
 // @author				X.Da
 // @create				2018-06-10
-// @version				0.7.5
+// @version				0.7.6
 // @match				*://beian.chinamobile.com/*
 // @match				*://10.1.68.22/*
 // @match				*://10.1.68.37/*
 // @namespace			beian-chinamobile
 // @license				MIT
 // @copyright			2018, X.Da
-// @lastmodified		2018-09-13
+// @lastmodified		2018-09-20
 // @feedback-url		https://greasyfork.org/scripts/369426
+// @note				2018-09-20-V0.7.6	fixed autoDelAndPost logic
 // @note				2018-09-13-V0.7.5	update for autoDelAndPost method
 // @note				2018-09-12-V0.7.1	update for autoDelAndPost method
 // @note				2018-09-06-V0.7.0	test for autoDelAndPost method
@@ -43,7 +44,7 @@
 (function () {
 	'use strict';
 
-	var devVersion = "0.7.5";
+	var devVersion = "0.7.6";
 
 	// Ajax 特效
 	$("body").append('<style>.head{background:#94aedb}#load{position:absolute;top:0;bottom:0;left:0;right:0;z-index:200;}#load ._close{position:absolute;bottom:20px;left:0;height:50px;width:50px;font-size:100px;color:#000;cursor:pointer;line-height:50px;opacity:.2}.spinner{position:absolute;top:50%;left:50%;margin-top:-100px;margin-left:-300px;text-align:center}.spinner>div{width:200px;height:200px;background-color:#67CF22;border-radius:100%;display:inline-block;-webkit-animation:bouncedelay 1.4s infinite ease-in-out;animation:bouncedelay 1.4s infinite ease-in-out;-webkit-animation-fill-mode:both;animation-fill-mode:both}.spinner .bounce1{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce2{-webkit-animation-delay:-.16s;animation-delay:-.16s}@-webkit-keyframes bouncedelay{0%,80%,100%{-webkit-transform:scale(0)}40%{-webkit-transform:scale(1)}}@keyframes bouncedelay{0%,80%,100%{transform:scale(0);-webkit-transform:scale(0)}40%{transform:scale(1);-webkit-transform:scale(1)}}</style><div id="load"><div class="_close" onclick="document.getElementById(&quot;load&quot;).style.display=&quot;none&quot;">×</div><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
@@ -123,7 +124,7 @@
 		}
 		if (notQuery && !/&/.test(__data)) {
 			// 非查询且 checkbox 无勾选
-			console.error("%c非查询且 checkbox 无勾选。已停止操作。","color:#088;font-size:18px");
+			console.error(url+"\t%c非查询操作且 checkbox 无勾选。已停止操作。", "color:#088;font-size:18px");
 			return;
 		}
 		$("#xda_temp").load(searchUrl, __data, function (data) {
@@ -159,15 +160,19 @@
 			var c_1 = $("#data_table tr.user-data:eq(" + i + ") td.c_1").text().trim();
 			var ipstr = $("#data_table tr.user-data:eq(" + i + ") td.c_2").text().trim() + "-" + $("#data_table tr.user-data:eq(" + i + ") td.c_3").text().trim();
 			var c_4 = $("#data_table tr.user-data:eq(" + i + ") td.c_4").text().trim();
-
 			if (c_4 == '自用') {
+				// 收集[自用] 到 data[]
 				data.push({ "id": c_1, "ipstr": ipstr, "index": i });
 			} else {
 				for (var j in data) {
 					if (data[j]['ipstr'] == ipstr) {
-						// 同 IP 重复出现，自用的勾选删除
-						$("#data_table input[name='sel'][value=" + data[j]["id"] + "]").attr("checked", true);
-						justPost = false;
+						// 同 IP 重复出现，且自用的可以勾选删除
+						var _checkbox = $("#data_table input[name='sel'][value=" + data[j]["id"] + "]");
+						// 排除已删除未上报的情况
+						if (_checkbox.length > 0) {
+							_checkbox.attr("checked", true);
+							justPost = false;
+						}
 						// sel_commit += "&sel_commit=" + data[j]["id"];
 						// sel_commit += "&sel_commit=" + c_1;
 					}
@@ -177,7 +182,7 @@
 		if (justPost) {
 			// 仅上报
 			ChooseAll_comm('sel_commit');
-			setTimeout(commitsubmit, 0);
+			setTimeout(commitsubmit, 100);
 		} else {
 			// 需要先删除
 			silencePost("fp_xx_delete.jhtml", del_fm);
